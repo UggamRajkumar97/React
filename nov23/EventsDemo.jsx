902dbd07a3a1ad334e25ab9214e379f13603673a
Rajkumar <138861148+UggamRajkumar97@users.noreply.github.com>
2023-11-23 12:28:57 +0530

    Add files via upload

--- nov23/EventsDemo.jsx
+++ nov23/EventsDemo.jsx
@@ -4,15 +4,26 @@ class Events extends Component {
     state = { 
         count:0
      } 
-     pressButton=()=>{
-        this.setState({count:this.state.count+1})
+     incrementButton=()=>{
+        if(this.state.count<10){
+            this.setState({count:this.state.count+1})
+        }
+        
+     }
+     decrementButton=()=>{
+        if(this.state.count>0){
+            this.setState({count:this.state.count-1})
+        }
+       
      }
     render() { 
-        
+
         return (
             <div>
-                <span className='badge bg-success m-4'>{this.state.count}</span>
-                <button className='badge bg-primary' onClick={this.pressButton}>Increment</button>
+                <button className='badge bg-primary' onClick={this.decrementButton}>-</button>
+                <span className='badge bg-success m-2'>{this.state.count}</span>
+                <button className='badge bg-primary' onClick={this.incrementButton}>+</button>
+
             </div>
         );
     }
