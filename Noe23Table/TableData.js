1eb7e3f3110cc823506e04fec3946a3cb94df824
Rajkumar <138861148+UggamRajkumar97@users.noreply.github.com>
2023-11-23 20:04:10 +0530

    Add files via upload

---
+++ Noe23Table/TableData.js
@@ -0,0 +1,19 @@
+const Employee=[
+    {
+        "id":101,
+        "name":"Karna",
+        "salary":999
+    },
+    {
+        "id":102,
+        "name":"Dharma",
+        "salary":888
+    },
+    {
+        "id":103,
+        "name":"Arjuna",
+        "salary":777
+    }
+]
+ 
+export default Employee;
