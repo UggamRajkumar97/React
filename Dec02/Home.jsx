3ffe0fd839b8609179ebed7c1c9a8ce6da0d60fc
Rajkumar <138861148+UggamRajkumar97@users.noreply.github.com>
2023-12-02 12:14:29 +0530

    Add files via upload

---
+++ Dec02/Home.jsx
@@ -0,0 +1,8 @@
+import React, { Component } from 'react'
+const Home = () => {
+    return ( 
+        <div>Home page</div>
+     );
+}
+ 
+export default Home;
---
+++ Dec02/navBar.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+const MyNavBar = () => {
+    return ( 
+        <ul>   
+            <li><a href="/home">Home</a></li>
+            <li><a href="/about">About</a></li>
+            <li><a href="/posts">Posts</a></li>
+            <li><a href="/contact">ContactUS</a></li>         
+       </ul>
+     );
+}
+ 
+export default MyNavBar;
